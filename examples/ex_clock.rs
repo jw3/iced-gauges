17d6e3426fb765d7cd56ced740fdcbb561334a18
John Wass <jwass3@gmail.com>
2023-04-22 05:55:07 -0400

    Example layering
    At the cost of some duplication the example clock can be broken into layers that make efficient use of caching.
    
    The duplication will be reduced in some way.

--- examples/ex_clock.rs
+++ examples/ex_clock.rs
@@ -16,7 +16,9 @@ fn main() -> iced::Result {
 
 struct Gauge {
     v: time::OffsetDateTime,
-    g: Cache,
+    hands: Cache,
+    bg: Cache,
+    frame: Cache,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -35,7 +37,9 @@ impl Application for Gauge {
             Gauge {
                 v: time::OffsetDateTime::now_local()
                     .unwrap_or_else(|_| time::OffsetDateTime::now_utc()),
-                g: Default::default(),
+                hands: Default::default(),
+                bg: Default::default(),
+                frame: Default::default(),
             },
             Command::none(),
         )
@@ -51,7 +55,7 @@ impl Application for Gauge {
                 let now = local_time;
                 if now != self.v {
                     self.v = now;
-                    self.g.clear();
+                    self.hands.clear();
                 }
             }
         }
@@ -87,12 +91,20 @@ impl canvas::Program<Msg> for Gauge {
         bounds: Rectangle,
         _cursor: Cursor,
     ) -> Vec<Geometry> {
-        let g = self.g.draw(bounds.size(), |frame| {
+        let bg = self.bg.draw(bounds.size(), |frame| {
+            println!("drawing bg");
             let center = frame.center();
             let radius = frame.width().min(frame.height()) / 2.5;
 
             let background = Path::circle(center, radius);
             frame.fill(&background, Color::from_rgb8(0x12, 0x93, 0xD8));
+        });
+
+        let g = self.hands.draw(bounds.size(), |frame| {
+            println!("drawing hands");
+
+            let center = frame.center();
+            let radius = frame.width().min(frame.height()) / 2.5;
 
             let width = radius / 100.0;
             let thin_stroke = |color: Color| -> Stroke {
@@ -113,81 +125,112 @@ impl canvas::Program<Msg> for Gauge {
                 }
             };
 
+            let length = PI * 2.0;
+            let step_12 = length / 12.0;
+            let step_60 = length / 60.0;
+
+            let short_hand = Path::line(Point::ORIGIN, Point::new(0.0, -0.5 * radius));
+            let long_hand = Path::line(Point::ORIGIN, Point::new(0.0, -0.8 * radius));
+
+            frame.translate(Vector::new(center.x, center.y));
+
+            frame.with_save(|frame| {
+                frame.rotate(self.v.hour() as f32 * step_12);
+                frame.stroke(&short_hand, wide_stroke(Color::WHITE));
+
+                frame.translate(Vector::new(0.0, -0.5 * radius));
+                frame.fill_text(format!("{}", self.v.hour()));
+            });
+
+            frame.with_save(|frame| {
+                frame.rotate(self.v.minute() as f32 * step_60);
+                frame.stroke(&long_hand, wide_stroke(Color::WHITE));
+
+                frame.translate(Vector::new(0.0, -0.8 * radius));
+                frame.fill_text(format!("{}", self.v.minute()));
+            });
+
+            frame.with_save(|frame| {
+                frame.rotate(self.v.second() as f32 * step_60);
+                frame.stroke(&long_hand, thin_stroke(Color::WHITE));
+
+                frame.translate(Vector::new(0.0, -0.8 * radius));
+                frame.fill_text(format!("{}", self.v.second()));
+            });
+        });
+
+        let frame = self.frame.draw(bounds.size(), |frame| {
+            println!("drawing frame");
+
+            let center = frame.center();
+            let radius = frame.width().min(frame.height()) / 2.5;
+            let width = radius / 100.0;
+
+            let thin_stroke = |color: Color| -> Stroke {
+                Stroke {
+                    width,
+                    style: stroke::Style::Solid(color),
+                    line_cap: LineCap::Round,
+                    ..Stroke::default()
+                }
+            };
+
             frame.with_save(|frame| {
                 frame.translate(Vector::new(center.x, center.y));
 
                 let length = PI * 2.0;
                 let step_12 = length / 12.0;
-                let step_60 = length / 60.0;
 
-                frame.with_save(|frame| {
-                    frame.rotate(PI * 1.5);
+                frame.rotate(PI * 1.5);
 
-                    let outer = Ellipse {
-                        center: Point::ORIGIN,
-                        major_curvature: 1.0 / radius,
-                        minor_radius: radius,
-                        angle: 0.0,
-                    };
+                let outer = Ellipse {
+                    center: Point::ORIGIN,
+                    major_curvature: 1.0 / radius,
+                    minor_radius: radius,
+                    angle: 0.0,
+                };
 
-                    let radius2 = radius - 25.0;
-                    let inner = Ellipse {
-                        center: Point::ORIGIN,
-                        major_curvature: 1.0 / radius2,
-                        minor_radius: radius2,
-                        angle: 0.0,
-                    };
+                let radius2 = radius - 25.0;
+                let inner = Ellipse {
+                    center: Point::ORIGIN,
+                    major_curvature: 1.0 / radius2,
+                    minor_radius: radius2,
+                    angle: 0.0,
+                };
 
-                    // hour ticks
-                    for a in 0..12 {
-                        let angle = step_12 * a as f32;
-                        let p1 = inner.get_point(angle);
-                        let p2 = outer.get_point(angle);
+                // hour ticks
+                for a in 0..12 {
+                    let angle = step_12 * a as f32;
+                    let p1 = inner.get_point(angle);
+                    let p2 = outer.get_point(angle);
 
-                        // println!("{a} {angle} {x1} {y1}, {x2} {y2}");
-                        let tick = Path::line(p1, p2);
-                        frame.with_save(|frame| {
-                            frame.stroke(&tick, thin_stroke(Color::BLACK));
+                    let tick = Path::line(p1, p2);
+                    frame.with_save(|frame| {
+                        frame.stroke(&tick, thin_stroke(Color::BLACK));
 
-                            frame.translate(Vector::new(p1.x, p1.y));
-                            frame.fill_text(format!("{a}"));
-                        });
-                    }
-                });
-
-                let short_hand = Path::line(Point::ORIGIN, Point::new(0.0, -0.5 * radius));
-                let long_hand = Path::line(Point::ORIGIN, Point::new(0.0, -0.8 * radius));
-
-                frame.with_save(|frame| {
-                    frame.rotate(self.v.hour() as f32 * step_12);
-                    frame.stroke(&short_hand, wide_stroke(Color::WHITE));
-
-                    frame.translate(Vector::new(0.0, -0.5 * radius));
-                    frame.fill_text(format!("{}", self.v.hour()));
-                });
-
-                frame.with_save(|frame| {
-                    frame.rotate(self.v.minute() as f32 * step_60);
-                    frame.stroke(&long_hand, wide_stroke(Color::WHITE));
-
-                    frame.translate(Vector::new(0.0, -0.8 * radius));
-                    frame.fill_text(format!("{}", self.v.minute()));
-                });
-
-                frame.with_save(|frame| {
-                    frame.rotate(self.v.second() as f32 * step_60);
-                    frame.stroke(&long_hand, thin_stroke(Color::WHITE));
-
-                    frame.translate(Vector::new(0.0, -0.8 * radius));
-                    frame.fill_text(format!("{}", self.v.second()));
-                });
+                        frame.translate(Vector::new(p1.x, p1.y));
+                        frame.fill_text(format!("{a}"));
+                    });
+                }
             });
+
+            let width = radius / 100.0;
+            let thin_stroke = |color: Color| -> Stroke {
+                Stroke {
+                    width,
+                    style: stroke::Style::Solid(color),
+                    line_cap: LineCap::Round,
+                    ..Stroke::default()
+                }
+            };
 
             let dot = Path::circle(center, radius / 25.0);
             frame.fill(&dot, Color::BLACK);
+
+            let background = Path::circle(center, radius);
             frame.stroke(&background, thin_stroke(Color::BLACK));
         });
 
-        vec![g]
+        vec![bg, g, frame]
     }
 }
